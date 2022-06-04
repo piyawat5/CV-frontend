395ddcf69cf4f76ac02aeeaa9da64b5c8e5a85a4
piyawat5 <98152492+piyawat5@users.noreply.github.com>
2022-06-04 23:47:41 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -15,12 +15,31 @@ window.addEventListener("scroll", () => {
 let headOne = document.querySelector("h1");
 let main = document.querySelector("main");
 let footer = document.querySelector("footer");
+function pdfDownload() {
+  if (
+    headOne.innerHTML ===
+    "You can download PDF RESUME at the bottom of this website (click contact)."
+  ) {
+    headOne.innerHTML = "RESUME";
+  } else {
+    headOne.innerHTML =
+      "You can download PDF RESUME at the bottom of this website (click contact).";
+  }
+}
+function pdfDownload2() {
+  if (headOne.innerHTML !== "RESUME") {
+    headOne.innerHTML = "RESUME";
+  }
+}
 headOne.addEventListener("click", () => {
+  pdfDownload();
   headOne.classList.toggle("big-head");
 });
 main.addEventListener("click", () => {
+  pdfDownload2();
   headOne.classList.remove("big-head");
 });
 footer.addEventListener("click", () => {
+  pdfDownload2();
   headOne.classList.remove("big-head");
 });
